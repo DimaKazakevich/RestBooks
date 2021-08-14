0c99a37ee7ed3051942eb1f8dbf88d3857d2b3b9
Dmitry Kazakevich <d.kazakevich@syberry.com>
2021-08-14 12:44:42 +0300

    remove unused comments and imports

--- src/main/java/by/kazakevich/restbooks/mapper/AuthorDtoToEntityMapper.java
+++ src/main/java/by/kazakevich/restbooks/mapper/AuthorDtoToEntityMapper.java
@@ -4,14 +4,11 @@ import by.kazakevich.restbooks.domain.Author;
 import by.kazakevich.restbooks.dto.request.CreateAuthorRequest;
 import by.kazakevich.restbooks.dto.response.AuthorResponse;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AuthorDtoToEntityMapper {
 
-  /*@Mapping(target = "country", source = "country")*/
   Author authorDtoToEntity(CreateAuthorRequest createAuthorRequest);
 
- /* @Mapping(target = "country", source = "country")*/
   AuthorResponse authorEntityToDto(Author author);
 }
--- src/main/java/by/kazakevich/restbooks/mapper/BookDtoToEntityMapper.java
+++ src/main/java/by/kazakevich/restbooks/mapper/BookDtoToEntityMapper.java
@@ -4,28 +4,11 @@ import by.kazakevich.restbooks.domain.Book;
 import by.kazakevich.restbooks.dto.request.CreateBookRequest;
 import by.kazakevich.restbooks.dto.response.BookResponse;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring")
 public interface BookDtoToEntityMapper {
 
-  /*@Mapping(target = "title", source = "title")*/
-/*  @Mappings({
-      @Mapping(target = "userId", ignore = true),
-      @Mapping(target = "authorId", ignore = true),
-      @Mapping(target = "user", ignore = true),
-      @Mapping(target = "author", ignore = true),
-  })*/
-  /*@Mapping(source = "title", target = "title")*/
   BookResponse bookEntityToDto(Book book);
 
-/*  @Mappings({
-      @Mapping(target = "userId", ignore = true),
-      @Mapping(target = "authorId", ignore = true),
-      @Mapping(target = "user", ignore = true),
-      @Mapping(target = "author", ignore = true),
-  })*/
-  /*@Mapping(source = "title", target = "title")*/
   Book bookDtoToEntity(CreateBookRequest createBookRequest);
 }
